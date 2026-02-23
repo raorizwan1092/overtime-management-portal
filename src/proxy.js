09c72a97b722ee2947ba68144a92799068d97f15
raorizwan1092 <raorizvi125@gmail.com>
2026-02-23 16:58:12 +0500

    updated proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { NAVIGATION_URLS } from "./constants/AppConstants";
+import { NAVIGATION_URLS,  } from "./constants/AppConstants";
 
 export function proxy(request) {
   const token = request.cookies.get("token")?.value;
@@ -7,11 +7,19 @@ export function proxy(request) {
 
   const isSignin = pathname === NAVIGATION_URLS.AUTH_URLS.SIGNIN;
   const isRoot = pathname === NAVIGATION_URLS.BASE_URL;
+  const protectedRoutes = ["/users", "/settings", "/time-log"];
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
   if (token && (isSignin || isRoot)) {
-    return NextResponse.redirect(new URL(NAVIGATION_URLS.USERS, request.url));
+    return NextResponse.redirect(
+      new URL(NAVIGATION_URLS.USERS, request.url)
+    );
   }
-  if (!token && (pathname.startsWith(NAVIGATION_URLS.USERS) || isRoot)) {
-    return NextResponse.redirect(new URL(NAVIGATION_URLS.AUTH_URLS.SIGNIN, request.url));
+  if (!token && (isProtectedRoute || isRoot)) {
+    return NextResponse.redirect(
+      new URL(NAVIGATION_URLS.AUTH_URLS.SIGNIN, request.url)
+    );
   }
 
   return NextResponse.next();
@@ -21,6 +29,8 @@ export const config = {
   matcher: [
     "/",
     "/signin",
-    "/users/:path*"
+    "/users/:path*",
+    "/settings/:path*",
+    "/time-log/:path*",
   ],
 };
